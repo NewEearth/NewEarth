f8fe7f28cd77b6efebc92e55d8804ccb136cdb25
hy219 <jhy_0219@daum.net>
2022-10-17 15:07:24 +0900

    firebase google 로그인 api 연동

--- public/main.js
+++ public/main.js
@@ -1,25 +1,3 @@
-//script.js
-import { loginEmail, signupEmail,loginGoogle } from './firebase.js';
-const buttons = document.getElementById('buttons');
-
-//Email 로그인, 회원가입 구현
-//...
-
-//Google 로그인
-google.addEventListener('click', (e) => {
-  loginGoogle().then((result) => {
-    console.log(result);
-    const user = result.user;
-    loginSuccess(user.email, user.uid);
-  });
-});
-
-//로그인 성공시 UI 변경
-const loginSuccess = (email, uid) => {
-  const login_area = document.getElementById('login-area');
-  login_area.innerHTML = `<h2>Login 성공!</h2><div>uid: ${uid}</div><div>email: ${email}</div>`;
-};
-
 /** top_list */
 const location_env_record = document.querySelectorAll(".location_env_record");
 const location_env_protect_record = document.querySelectorAll(".location_env_protect_record")
