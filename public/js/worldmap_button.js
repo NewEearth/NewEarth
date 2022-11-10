65f3fbdf307b91737178994b8567b59510dd012d
hy219 <jhy_0219@daum.net>
2022-11-10 21:04:25 +0900

    환경지도 추가 및 화면전환 완(지구 클릭 시 환경지도로 이동하도록 추가해야함)

---
+++ public/js/worldmap_button.js
@@ -0,0 +1,14 @@
+document.getElementById("btn_community").addEventListener("click", () => {
+    console.log("환경일지 페이지로 이동");
+    window.location.href='./communityUI_환경일지목록.html'
+});
+
+document.getElementById("btn_Quest").addEventListener("click", () => {
+    console.log("환경발자국 페이지로 이동");
+    window.location.href='./content_env_step.html'
+});
+
+document.getElementById("btn_Activites").addEventListener("click", () => {
+    console.log("환경보호방법 페이지로 이동");
+    window.location.href='./communityUI_환경보호방법목록.html'
+});
